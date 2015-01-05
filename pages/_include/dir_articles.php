65b94d3a4952b7bb98a369a22a1fd191029af407
Andrew_2212 <6612273@gmail.com>
2015-01-05 12:08:34 +0200

    NationID page is fixed

--- pages/_include/dir_articles.php
+++ pages/_include/dir_articles.php
@@ -5,10 +5,13 @@
           <h4>Статьи</h4>
 
         <li>
-          <a href="book_mova.php">Хорошая горькая книга</a>
+          <a target="_blank" href="http://nn.by/?c=ar&i=125014&lang=ru">Откровения националиста [nn.by Лучшее за 2014 г.]</a>
         </li>
         <li>
-          <a href="http://www.charter97.org/ru/news/2013/9/18/75920/">Белорусоцентристский взгляд на историю</a>
+          <a href="nation_id.php">Национальная идентификация</a>
+        </li>
+        <li>
+          <a target="_blank" href="http://www.charter97.org/ru/news/2013/9/18/75920/">Белорусоцентристский взгляд на историю</a>
         </li>
         <li>
           <a href="rus_mir.php">МГУ им. Ломоносова: <i>— "Никакой белорусской традиции нет"</i></a>
@@ -26,7 +29,7 @@
           <a href="blr_family_names.php">Наши фамилии</a>
         </li>
         <li>
-          <a href="nation_id.php">Национальная идентификация</a>
+          <a href="book_mova.php">Хорошая горькая книга</a>
         </li>
 		<li>
           <a href="#" class="item-unable">Нация — элита vs крестьяство</a>
